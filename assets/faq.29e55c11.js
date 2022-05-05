3f1bbb2f56aabc7f06e083db6654b9b13f0bb4d2
Anders Åberg <anders@andersaberg.com>
2022-05-05 09:24:43 +0200

    deploy

---
+++ assets/faq.29e55c11.js
@@ -0,0 +1,72 @@
+import"./globe.a7885a3f.js";import"./vendor.2f0b8dd2.js";const e=`<h1>FAQ</h1>
+<h3>\u{1F449} Hur mycket av pengarna g\xE5r till \xE4ndam\xE5let?</h3>
+<p>\xC4ven fast vi inte har ett 90-konto t\xE4nker vi efterstr\xE4va samma krav. Kostnader f\xF6r administration och insamling f\xE5r max utg\xF6ra 25% av int\xE4kterna.
+Detta garanterar vi i de partneravtal vi skriver. V\xE5rt m\xE5l \xE4r att administration och insamlingskostnader \xE4r s\xE5 l\xE5ga som m\xF6jligt.</p>
+<h2>Partnerf\xF6retag</h2>
+<h3>\u{1F449} Vad kr\xE4vs av oss?</h3>
+<p>Att vara partner \xE4r underh\xE5llsfritt och enkelt. Vi beh\xF6ver en e-postadress och en logga f\xF6r att komma ig\xE5ng. Avgiften betalas med en \xE5rsfaktura.</p>
+<h3>\u{1F449} Vilka open source-utvecklare blir sponsrade?</h3>
+<p>Alla utvecklare som blir sponsrade \xE4r utvalda och kvalitetsgranskade av Svensk Open Source. Ans\xF6kan \xE4r \xF6ppen hela \xE5ret och varje v\xE5r/h\xF6st tar vi in nya utvecklare och f\xF6rdelar sponsringen.</p>
+<h3>\u{1F449} Kan vi se vilka open source-utvecklare vi sponsrar? Kan vi visa andra vad vi sponsrar?</h3>
+<p>Ja! P\xE5 din publika partnerprofil visas en sammanst\xE4llning p\xE5 hur l\xE4nge ni varit partners, totalt belopp och vilka projekt och utecklare som dragit nytta av er sponsring.
+Partnerprofilen g\xE5r \xE4ven att dela publikt.</p>
+<p>Du f\xE5r \xE4ven l\xF6pande sammanst\xE4llningar p\xE5 ditt partnerskap och hur det g\xE5r i de projekt du sponsrar.</p>
+<h3>\u{1F449} Vad har ni f\xF6r etiska riktlinjer och krav p\xE5 projekt som f\xE5r del av pengarna?</h3>
+<p>V\xE5rt sponsorprogram f\xF6rh\xE5ller sig till samma etiska riktlinjer som barncancerfonden.
+Du som utvecklare f\xE5r alltid information om vilka f\xF6retag som \xE4r del av din sponsring och kommer ha m\xF6jligheten att tacka nej.
+L\xE4s v\xE5ra etiska riktlinjer f\xF6r insamling (l\xE4nk)</p>
+<h2>Utvecklare</h2>
+<h3>\u{1F449} Hur blir en utvecklare eller projekt sponsrat?</h3>
+<p>F\xF6r tillf\xE4llet sponsrar vi alltid en person f\xF6r arbetet p\xE5 ett specifikt projekt.
+Du som utvecklar eller bidrar till ett projekt ans\xF6ker om att bli sponsrade. (l\xE4nk till ans\xF6kan formul\xE4r)</p>
+<p>Personens bidrag till projektet beh\xF6ver vara betydande (t.ex. Core-maintainer) och aktivt. Arbetsinsatsen och kvalit\xE9 p\xE5 projektet utv\xE4rderas av en grupp seniora utvecklare knutna till Svensk Open Source.</p>
+<p>Personen beh\xF6ver vara svensk medborgare, men projektet beh\xF6ver inte vara &quot;svenskt&quot;.</p>
+<h3>\u{1F449} Vilken motprestation m\xE5ste jag g\xF6ra f\xF6r att bli sponsrad?</h3>
+<p>F\xF6r att bli sponsrad m\xE5te man g\xF6ra en motprestation. Det allra vanligast \xE4r att presentera sponsorer p\xE5 projektets GitHub-sida eller hemsida.
+Du f\xE5r en unik url och bild som automatiskt visar dina sponsorer.</p>
+<p>Om inb\xE4ddning inte \xE4r m\xF6jlig p\xE5 projekt s\xE5 utforskar vi m\xF6jliga kompromiser tillsammans med utvecklare. Niv\xE5n av sponsring kan justeras f\xF6r att passa den motprestation som \xE4r m\xF6jlig.</p>
+<h3>\u{1F449} Hur betalas pengarna ut?</h3>
+<p>Vi anv\xE4nder oss av Github sponsors f\xF6r utbetalning. P\xE5 det s\xE4ttet minskar vi n\xF6dv\xE4ndigt pappersarbete, b\xE5de f\xF6r dig och oss.</p>
+<h3>\u{1F449} Hur ser ni till att inte gamblingf\xF6retag sponsrar projekt som inte accepterar s\xE5dana f\xF6retag som sponsorer?</h3>
+<p>V\xE5rt sponsorprogram f\xF6rh\xE5ller sig till samma etiska riktlinjer som barncancerfonden.
+Du som utvecklare f\xE5r alltid information om vilka f\xF6retag som \xE4r del av din sponsring och kommer ha m\xF6jligheten att tacka nej.
+L\xE4s v\xE5ra etiska riktlinjer f\xF6r insamling (l\xE4nk)</p>
+<h3>\u{1F449} Vad h\xE4nder om n\xE5gon forkar mitt projekt och ocks\xE5 blir medlem?</h3>
+<p>Om n\xE5gon forkar ditt projekt beh\xF6ver den personen ans\xF6ka om att bli sponsrad. Din sponsring p\xE5verkas inte.</p>
+<h3>\u{1F449} Hur f\xF6rdelas pengar bland de utvecklare som \xE4r sponrade? F\xE5r alla samma belopp?</h3>
+<p>Sponsorbeloppet justeras enligt flera faktorer.
+Dels s\xE5 kan en partner v\xE4lja att vikta sin sponsring s\xE5 att mer st\xF6d g\xE5r till vissa kategorier och ekosystem, t.ex. &quot;.net&quot;, &quot;erlang&quot; eller &quot;game development&quot;.
+Beloppet justeras ocks\xE5 beroende p\xE5 ditt engagemang i projektet, aktivitetniv\xE5 och projektets omfattning.</p>
+<p>Skiss f\xF6rdelningsexempel, b\xF6r vara fl\xF6desschema:</p>
+<p>Pengar f\xF6redlas p\xE5 f\xF6ljande s\xE4tt:
+Bolag A ansluter sig som partner, 100 000kr/\xE5r.
+Bolag A anm\xE4ler ett speciellt intresse i (.NET och Javascript.)
+Det finns 10 anslutna utvecklare. 2 inom .NET och 4 inom JS och 4 \xF6vriga.</p>
+<p>50% av beloppet g\xE5r till den den &quot;allm\xE4na f\xF6rdelningen&quot;. 50 000kr
+50% f\xF6rdelas enligt intresse-viktning: 25 000 .NET och 25 000 Javascript.</p>
+<p>I exmeplet f\xE5r alla anslutna utvecklare lika delar av den allm\xE4nga f\xF6rdelningen. I s\xE4song tv\xE5 sker viktning L\xE5g/Medel/H\xF6g f\xF6r att s\xE4rskilja projekts dignitet / verkh\xF6jd.
+50 000kr / 10 utecklare = 5 000kr / utvecklare</p>
+<ul>
+<li>Intressef\xF6rdelning:
+25 000kr .net / 2 .net utvecklare = +12 500 kr / .net utvecklare
+25 000kr .js / 4 .js utvecklare = +6 250 kr / .js utvecklare</li>
+</ul>
+<p>Total sponsring:</p>
+<p>5000 kr + 12 500kr = 17 500kr per .net utvecklare per \xE5r
+5000 kr + 6 250kr = 11 250kr per .js utvecklare per \xE5r</p>
+<p>Fr\xE5n bolag A.</p>
+<h1>Etiska riktlinjer f\xF6r insamling</h1>
+<p>Svensk Open Source f\xF6rbeh\xE5ller sig r\xE4tten att tacka nej till partnerskap, samarbeten och insamlingar som inte \xF6verensst\xE4mmer med v\xE5ra grundl\xE4ggande v\xE4rderingar eller p\xE5 annat s\xE4tt kan t\xE4nkas skada verksamheten, organisationen, v\xE5rt renomm\xE9 eller varum\xE4rke.</p>
+<p>Etiska riktlinjer f\xF6r insamling</p>
+<p>Svensk Open Source tar inte emot g\xE5vor fr\xE5n och samarbetar inte med f\xF6retag eller organisationer som:</p>
+<ul>
+<li>medverkar i olaglig, korrupt eller oetisk verksamhet eller \xE4r utsatt f\xF6r sanktioner fr\xE5n FN, EU eller USA.</li>
+<li>har sin verksamhet inom vapen- eller pornografibranschen.</li>
+<li>har sin huvudsakliga verksamhet inom alkohol- eller tobak- eller spelbranschen (undantaget spelformen \u201Dspel f\xF6r allm\xE4nnyttiga \xE4ndam\xE5l\u201D).</li>
+<li>Vi \xE5terbetalar pengar om det kommer till v\xE5r vetskap att vi har f\xE5tt in g\xE5vor fr\xE5n f\xF6retag eller organisationer som beskrivs enligt ovan.</li>
+<li>V\xE5ra ambassad\xF6rer b\xF6r dela v\xE5r syn p\xE5 etik och inte sj\xE4lva ha samarbeten med f\xF6retag eller organisationer enligt ovan.</li>
+<li>Vi tar inte emot g\xE5vor fr\xE5n politiska partier, d\xE5 vi inte vill riskera att det kan uppfattas som att vi skulle styras av politiska intressen. Undantag \xE4r minnesg\xE5vor och anm\xE4lningsavgifter till event.</li>
+</ul>
+<p>Svensk Open Source kan ocks\xE5 neka till en g\xE5va om g\xE5vogivarens v\xE4rderingar eller verksamhet st\xE5r i strid med organisationens v\xE4rderingar samt om g\xE5vans ursprung strider mot riktlinjerna. Svensk Open Source kan \xE4ven tacka nej till en g\xE5va om den \xE4r f\xF6renad med villkor som Svensk Open Source inte kan uppfylla eller om kostnaderna f\xF6r g\xE5van inte st\xE5r i rimlig proportion till g\xE5van i sig.</p>
+`;document.getElementById("faq").innerHTML=e;
+//# sourceMappingURL=faq.29e55c11.js.map
