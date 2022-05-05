4446c7b7edeeb6190241aba20752e736ad743b7c
Anders Åberg <anders@andersaberg.com>
2022-05-05 09:02:30 +0200

    faq

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-/**
-* @type {import('vite').UserConfig}
-*/
-export default {
-    build: {
-        sourcemap: true,
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        partner: resolve(__dirname, 'partner.html'),
+        utvecklare: resolve(__dirname, 'utvecklare.html'),
+        faq: resolve(__dirname, 'faq.html'),
+      }
     },
-}
+    sourcemap: true
+  }
+})
