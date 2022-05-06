7ed778be88cb81f49ae62a355378763dbbe3477c
Anders Åberg <anders@andersaberg.com>
2022-05-06 15:40:15 +0200

    deploy

--- assets/faq.1cf0ce8a.js
+++ assets/faq.1cf0ce8a.js
@@ -23,8 +23,8 @@ Du som utvecklar eller bidrar till ett projekt ans\xF6ker om att bli sponsrade. 
 <p>Personen beh\xF6ver vara svensk medborgare, men projektet beh\xF6ver inte vara &quot;svenskt&quot;.</p>
 <h3>\u{1F449} Vilken motprestation m\xE5ste jag g\xF6ra f\xF6r att bli sponsrad?</h3>
 <p>F\xF6r att bli sponsrad m\xE5te man g\xF6ra en motprestation. Det allra vanligast \xE4r att presentera sponsorer p\xE5 projektets GitHub-sida eller hemsida.
-Du f\xE5r en unik url och bild som automatiskt visar dina sponsorer.</p>
-<p>Om inb\xE4ddning inte \xE4r m\xF6jlig p\xE5 projekt s\xE5 utforskar vi m\xF6jliga kompromiser tillsammans med utvecklare. Niv\xE5n av sponsring kan justeras f\xF6r att passa den motprestation som \xE4r m\xF6jlig.</p>
+Du f\xE5r en unik url och bild som automatiskt visar dina sponsorer, exempel: <code>https://img.svenskopensource.se/sponsors/[username]</code></p>
+<p>Om inb\xE4ddning inte \xE4r m\xF6jlig p\xE5 projekt s\xE5 utforskar vi m\xF6jliga kompromiser tillsammans med utvecklaren i fr\xE5ga. Niv\xE5n av sponsring kan justeras f\xF6r att passa den motprestation som \xE4r m\xF6jlig.</p>
 <h3>\u{1F449} Hur betalas pengarna ut?</h3>
 <p>Vi anv\xE4nder oss av Github sponsors f\xF6r utbetalning. P\xE5 det s\xE4ttet minskar vi n\xF6dv\xE4ndigt pappersarbete, b\xE5de f\xF6r dig och oss.</p>
 <h3>\u{1F449} Hur ser ni till att inte gamblingf\xF6retag sponsrar projekt som inte accepterar s\xE5dana f\xF6retag som sponsorer?</h3>
@@ -37,23 +37,25 @@ L\xE4s v\xE5ra etiska riktlinjer f\xF6r insamling (l\xE4nk)</p>
 <p>Sponsorbeloppet justeras enligt flera faktorer.
 Dels kan en partner v\xE4lja att vikta sin sponsring s\xE5 att mer st\xF6d g\xE5r till vissa kategorier och ekosystem, t.ex. &quot;.net&quot;, &quot;erlang&quot; eller &quot;game development&quot;.
 Beloppet justeras ocks\xE5 beroende p\xE5 ditt engagemang i projektet, aktivitetniv\xE5 och projektets omfattning.</p>
-<p>Skiss f\xF6rdelningsexempel, b\xF6r vara fl\xF6desschema:</p>
-<p>Pengar f\xF6redlas p\xE5 f\xF6ljande s\xE4tt:
-Bolag A ansluter sig som partner, 100 000kr/\xE5r.
-Bolag A anm\xE4ler ett speciellt intresse i (.NET och Javascript.)
+<p>Utkast p\xE5 f\xF6rdelning av medel, s\xE4song 1:</p>
+<p>Pengar f\xF6redlas p\xE5 f\xF6ljande s\xE4tt:<br>
+Bolag A ansluter sig som partner, 100 000kr/\xE5r.<br>
+Bolag A anm\xE4ler ett speciellt intresse i (.NET och Javascript.)<br>
 Det finns 10 anslutna utvecklare. 2 inom .NET och 4 inom JS och 4 \xF6vriga.</p>
-<p>50% av beloppet g\xE5r till den den &quot;allm\xE4na f\xF6rdelningen&quot;. 50 000kr
+<p>50% av beloppet g\xE5r till den den &quot;allm\xE4na f\xF6rdelningen&quot;. 50 000kr<br>
 50% f\xF6rdelas enligt intresse-viktning: 25 000 .NET och 25 000 Javascript.</p>
-<p>I exmeplet f\xE5r alla anslutna utvecklare lika delar av den allm\xE4nga f\xF6rdelningen. I s\xE4song tv\xE5 sker viktning L\xE5g/Medel/H\xF6g f\xF6r att s\xE4rskilja projekts dignitet / verkh\xF6jd.
+<p>I exmeplet f\xE5r alla anslutna utvecklare lika delar av den allm\xE4nga f\xF6rdelningen. I s\xE4song tv\xE5 sker viktning L\xE5g/Medel/H\xF6g f\xF6r att s\xE4rskilja projekts dignitet / verkh\xF6jd.<br>
 50 000kr / 10 utecklare = 5 000kr / utvecklare</p>
+<p>+ Intressef\xF6rdelning:</p>
 <ul>
-<li>Intressef\xF6rdelning:
-25 000kr .net / 2 .net utvecklare = +12 500 kr / .net utvecklare
-25 000kr .js / 4 .js utvecklare = +6 250 kr / .js utvecklare</li>
+<li>25 000kr .net / 2 .net utvecklare = +12 500 kr / .net utvecklare</li>
+<li>25 000kr .js / 4 .js utvecklare = +6 250 kr / .js utvecklare</li>
 </ul>
 <p>Total sponsring:</p>
-<p>5000 kr + 12 500kr = 17 500kr per .net utvecklare per \xE5r
-5000 kr + 6 250kr = 11 250kr per .js utvecklare per \xE5r</p>
+<ul>
+<li>5000 kr + 12 500kr = <strong>17 500kr per .net utvecklare per \xE5r</strong></li>
+<li>5000 kr + 6 250kr = <strong>11 250kr per .js utvecklare per \xE5r</strong></li>
+</ul>
 <p>Fr\xE5n bolag A.</p>
 <h1>Etiska riktlinjer f\xF6r insamling</h1>
 <p>Svensk Open Source f\xF6rbeh\xE5ller sig r\xE4tten att tacka nej till partnerskap, samarbeten och insamlingar som inte \xF6verensst\xE4mmer med v\xE5ra grundl\xE4ggande v\xE4rderingar eller p\xE5 annat s\xE4tt kan t\xE4nkas skada verksamheten, organisationen, v\xE5rt renomm\xE9 eller varum\xE4rke.</p>
@@ -62,10 +64,10 @@ Det finns 10 anslutna utvecklare. 2 inom .NET och 4 inom JS och 4 \xF6vriga.</p>
 <ul>
 <li>medverkar i olaglig, korrupt eller oetisk verksamhet eller \xE4r utsatt f\xF6r sanktioner fr\xE5n FN, EU eller USA.</li>
 <li>har sin verksamhet inom vapen- eller pornografibranschen.</li>
-<li>har sin huvudsakliga verksamhet inom alkohol- eller tobak- eller spelbranschen (undantaget spelformen \u201Dspel f\xF6r allm\xE4nnyttiga \xE4ndam\xE5l\u201D).</li>
+<li>har sin huvudsakliga verksamhet inom alkohol- eller tobak- eller gambling- eller casinobranschen (undantaget \u201Dspel f\xF6r allm\xE4nnyttiga \xE4ndam\xE5l\u201D).</li>
 <li>Vi \xE5terbetalar pengar om det kommer till v\xE5r vetskap att vi har f\xE5tt in g\xE5vor fr\xE5n f\xF6retag eller organisationer som beskrivs enligt ovan.</li>
 <li>V\xE5ra ambassad\xF6rer b\xF6r dela v\xE5r syn p\xE5 etik och inte sj\xE4lva ha samarbeten med f\xF6retag eller organisationer enligt ovan.</li>
 <li>Vi tar inte emot g\xE5vor fr\xE5n politiska partier, d\xE5 vi inte vill riskera att det kan uppfattas som att vi skulle styras av politiska intressen. Undantag \xE4r minnesg\xE5vor och anm\xE4lningsavgifter till event.</li>
 </ul>
 `;document.getElementById("faq").innerHTML=e;
-//# sourceMappingURL=faq.dcf2d141.js.map
+//# sourceMappingURL=faq.1cf0ce8a.js.map
